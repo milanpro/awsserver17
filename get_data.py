c0b3512e0d75e0227b31756b4136798727b6196e
Milan Proell <milan.proell@student.hpi.de>
2017-06-18 04:46:44 +0200

    sfasdf

--- get_data.py
+++ get_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 def LastDay(day):
     db = database_connector()
+    print(day)
     df = pd.read_sql("SELECT avg_heart_rate, sleep_duration, sleep_interrupts, coffein, calories, steps FROM data WHERE day = "+str(69-int(day))+";", db.engine)
     avg_hr = str(df.ix[0, 'avg_heart_rate'])
     slp_du = str(df.ix[0, 'sleep_duration'])
@@ -13,10 +14,8 @@ def LastDay(day):
     output = "Deine Herzfrequenz betrug heute im Durchschnitt "+avg_hr+" bpm,"\
                 "du bist "+steps+" Schritte gelaufen, hast "+slp_du+" Stunden geschlafen"\
                 "und "+cal+" Kalorien konsumiert."
-    """
     if(int(steps)<=2000):
         output = output + "Du solltest dich etwas mehr bewegen."
     else:
         output = output + "Du hast dich heute viel bewegt!"
-    """
     return output
